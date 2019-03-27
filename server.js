59b9ae4cff81fc7987f7b661a4f91d271186fd1a
Sanket <ssnavalakha@gmail.com>
2019-03-27 18:47:53 -0400

    server.js change

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static("./dist/student-angular-webdev-sp2019"));
 
 app.get("/*", function(req, res) {
 
-  res.sendFile(path.join(__dirname, "/dist/student-angular-webdev-sp2019/index.html"));
+  res.sendFile(path.join(__dirname + "/dist/student-angular-webdev-sp2019/index.html"));
 });
 
 // Start the app by listening on the default Heroku port
